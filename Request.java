103e259fdfe499b34c1f37e45651c69d4eb9820d
Muhammad Suleman <m.suleman275@gmail.com>
2022-06-05 02:04:53 +0300

    magic

---
+++ Request.java
@@ -0,0 +1,9 @@
+public class Request {
+   int type;
+   String payload;
+
+   public Request(int type, String payload) {
+      this.type = type;
+      this.payload = payload;
+   }
+}
--- Server.java
+++ Server.java
@@ -1,3 +1,56 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
-   
+   // variables
+   private ServerSocket serverSocket;
+
+   // functions
+
+   // start function
+   public void start(int port) throws IOException {
+      serverSocket = new ServerSocket(port);
+      while (true)
+         new EchoClientHandler(serverSocket.accept()).start();
+   }
+
+   // stop function
+   public void stop() throws IOException {
+      serverSocket.close();
+   }
+
+   // client handler for multithreading
+   private static class EchoClientHandler extends Thread {
+      private Socket clientSocket;
+      private PrintWriter out;
+      private BufferedReader in;
+
+      // constructor
+      public EchoClientHandler(Socket socket) {
+         this.clientSocket = socket;
+      }
+
+      public void run() {
+         try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+               if (".".equals(inputLine)) {
+                  out.println("bye");
+                  break;
+               }
+               out.println(inputLine);
+            }
+
+            in.close();
+            out.close();
+            clientSocket.close();
+         } catch (IOException e) {
+            e.toString();
+         }
+      }
+   }
 }
